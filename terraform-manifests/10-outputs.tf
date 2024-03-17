73830975573ae25ebff93a2ce5da7c22d405e2e2
idanhom <pson93@gmail.com>
2024-03-17 20:08:45 +0100

    changes to output

--- terraform-manifests/10-outputs.tf
+++ terraform-manifests/10-outputs.tf
@@ -13,3 +13,55 @@ output "virtual_network_name" {
   description = "Virutal Network Name"
   value       = azurerm_virtual_network.myvnet.id
 }
+# ------------------------
+# From 06-datasource, rg
+# 1. Resource Group Name from Datasource
+output "ds_rg_name" {
+  value = data.azurerm_resource_group.rgds.name
+}
+
+# 2. Resource Group Location from Datasource
+output "ds_rg_location" {
+  value = data.azurerm_resource_group.rgds.location
+}
+
+# 3. Resource Group ID from Datasource
+output "ds_rg_id" {
+  value = data.azurerm_resource_group.rgds.id
+}
+# -------------------
+# From 06-datasource, vnet
+
+# 1. Virtual Network Name from Datasource
+output "ds_vnet_name" {
+  value = data.azurerm_virtual_network.vnetds
+}
+
+# 2. Virtual Network ID from Datasource
+output "ds_vnet_id" {
+  value = data.azurerm_virtual_network.vnetds.id 
+}
+
+# 3. Virtual Network address_space from Datasource
+output "ds_vnet_address_space" {
+  value = data.azurerm_virtual_network.vnetds.address_space
+}
+
+# --------------
+
+# Subscription
+
+# 1. My Current Subscription Display Name
+output "current_subscription_display_name" {
+  value = data.azurerm_subscription.current.display_name
+}
+
+# 2. My Current Subscription Id
+output "current_subscription_id" {
+  value = data.azurerm_subscription.current.subscription_id
+}
+
+# 3. My Current Subscription Spending Limit
+output "current_subscription_spending_limit" {
+  value = data.azurerm_subscription.current.spending_limit
+}
