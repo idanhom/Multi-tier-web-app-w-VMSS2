1e86d4686493d5fc25d1aa4af0f02ed83e1623a1
idanhom <pson93@gmail.com>
2024-03-18 06:21:29 +0100

    changes to remote state saving config

--- terraform-manifests/02-backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "demo"
-    storage_account_name = "tfstate1231"
-    container_name       = "tfstate"
-    key                  = "demo.terraform.tfstate"
-  }
-}
---
+++ terraform-manifests/02-remote-state.tf
@@ -0,0 +1,13 @@
+data "terraform_remote_state" "remote_state" {
+  backend = "azurerm"
+
+  config = {
+    resource_group_name = "demo"
+    storage_account_name = "tfstate1231"
+    container_name = "tfstate"
+    key = "demo.terraform.tfstate"
+  }
+  
+
+
+}
