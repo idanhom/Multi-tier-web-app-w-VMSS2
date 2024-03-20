9f7eef5672dc5e743fe80846616bada9e9996b31
idanhom <pson93@gmail.com>
2024-03-20 06:37:17 +0100

    added comments

--- terraform-manifests/03-2-locals.tf
+++ terraform-manifests/03-2-locals.tf
@@ -3,65 +3,30 @@ locals {
   vnet_name = "${var.business_unit}-${var.environment}-${var.virtual_network_name}"
 }
 
+# Mapping of inbound ports for web tier - maps external ports to internal service ports
 locals {
   web_inbound_ports_map = {
-    "100" : "80"
-    "110" : "443"
-    "120" : "22"
-
+    "100" : "80"   # HTTP
+    "110" : "443"  # HTTPS
+    "120" : "22"   # SSH
   }
 }
 
+# Mapping of inbound ports for application tier - maps external ports to internal application ports
 locals {
   app_inbound_ports_map = {
-    "100" : "80",
-    "110" : "443",
-    "120" : "8080",
-    "130" : "22"
+    "100" : "80",   # HTTP
+    "110" : "443",  # HTTPS
+    "120" : "8080", # Alternative HTTP
+    "130" : "22"    # SSH
   } 
 }
 
+# Mapping of inbound ports for database tier - maps external ports to internal database ports
 locals {
   db_inbound_ports_map = {
-    "100" : "3306",
-    "110" : "1433",
-    "120" : "5432"
+    "100" : "3306", # MySQL
+    "110" : "1433", # MS SQL
+    "120" : "5432"  # PostgreSQL
   } 
 }
-
-
-
-# locals {
-#   nsg_rules = {
-#     allow-http = {
-#       priority                   = 100
-#       direction                  = "Inbound"
-#       access                     = "Allow"
-#       protocol                   = "Tcp"
-#       source_port_range          = "*"
-#       destination_port_range     = "80"
-#       source_address_prefix      = "*"
-#       destination_address_prefix = "*"
-#     },
-#     allow-https = {
-#       priority                   = 110
-#       direction                  = "Inbound"
-#       access                     = "Allow"
-#       protocol                   = "Tcp"
-#       source_port_range          = "*"
-#       destination_port_range     = "443"
-#       source_address_prefix      = "*"
-#       destination_address_prefix = "*"
-#     },
-#     allow-ssh = {
-#       priority                   = 120
-#       direction                  = "Inbound"
-#       access                     = "Allow"
-#       protocol                   = "Tcp"
-#       source_port_range          = "*"
-#       destination_port_range     = "22"
-#       source_address_prefix      = "YourPublicIP/32" # Replace with your actual public IP
-#       destination_address_prefix = "*"
-#     }
-#   }
-# }
