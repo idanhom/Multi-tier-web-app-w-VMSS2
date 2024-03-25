6dde93e6cb0a8537ab3c47831a52a38528abcd6f
idanhom <pson93@gmail.com>
2024-03-25 20:14:41 +0100

    lagt till load balancer. felsök pågår

--- terraform-manifests/07-01-web_linuxvm-publicip.tf
+++ terraform-manifests/07-01-web_linuxvm-publicip.tf
@@ -1,7 +1,7 @@
-resource "azurerm_public_ip" "publicip" {
-  name                = "${var.business_unit}-publicip"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  allocation_method   = "Static"
-}
+# resource "azurerm_public_ip" "publicip" {
+#   name                = "${local.resource_name_prefix}-web-linuxvm-publicip"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
+#   allocation_method   = "Static"
+# }
 
--- terraform-manifests/10-outputs.tf
+++ terraform-manifests/10-outputs.tf
@@ -1,24 +1,43 @@
 # Subscription
 
-# 1. My Current Subscription Display Name
+# My Current Subscription Display Name
 output "current_subscription_display_name" {
   description = "Display Name of the Current Azure Subscription"
   value       = data.azurerm_subscription.current.display_name
 }
 
-# 2. My Current Subscription Id
+# My Current Subscription Id
 output "current_subscription_id" {
   description = "ID of the Current Azure Subscription"
   value       = data.azurerm_subscription.current.subscription_id
 }
 
-# 3. My Current Subscription Spending Limit
+# My Current Subscription Spending Limit
 output "current_subscription_spending_limit" {
   description = "Spending Limit of the Current Azure Subscription"
   value       = data.azurerm_subscription.current.spending_limit
 }
 
-output "public_ip_address" {
-  description = "Public IP of web-linuxvm"
+
+# output "public_ip_address" {
+#   description = "Public IP of web-linuxvm"
+#   value = azurerm_public_ip.publicip.ip_address
+# }
+
+# LB Public IP
+output "web_lb_public_ip_address" {
+  description = "Web Load Balancer Public Address"
   value = azurerm_public_ip.publicip.ip_address
+}
+
+# Load Balancer ID
+output "web_lb_id" {
+  description = "Web Load Balancer ID."
+  value = azurerm_lb.web_lb.id
+}
+
+# Load Balancer Frontend IP Configuration Block
+output "web_lb_frontend_ip_configuration" {
+  description = "Web LB frontend_ip_configuration Block"
+  value = [azurerm_lb.web_lb.frontend_ip_configuration]
 }
