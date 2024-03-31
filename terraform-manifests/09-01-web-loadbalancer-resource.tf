8487fd895a2a070fa35c3d89dac4f88664ec83b3
idanhom <pson93@gmail.com>
2024-03-31 07:00:01 +0200

    added web lb

--- terraform-manifests/09-01-web-loadbalancer-resource.tf
+++ terraform-manifests/09-01-web-loadbalancer-resource.tf
@@ -1,4 +1,4 @@
-resource "azurerm_public_ip" "publicip" {
+resource "azurerm_public_ip" "web_publicip" {
   name                = "${local.resource_name_prefix}-lb-public-ip"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
@@ -13,7 +13,7 @@ resource "azurerm_lb" "web_lb" {
   sku                 = "Standard"
   frontend_ip_configuration {
     name                 = "web-lb-publicip-1"
-    public_ip_address_id = azurerm_public_ip.publicip.id
+    public_ip_address_id = azurerm_public_ip.web_publicip.id
   }
 }
 
@@ -23,14 +23,14 @@ resource "azurerm_lb_backend_address_pool" "web_lb_backend_address_pool" {
 }
 
 resource "azurerm_lb_probe" "web_lb_probe" {
-  name            = "tcp-probe"
+  name            = "web-tcp-probe"
   protocol        = "Tcp"
   port            = 80
   loadbalancer_id = azurerm_lb.web_lb.id
 }
 
-resource "azurerm_lb_rule" "web_lb_rule_app1" {
-  name                           = "web-app1-rule"
+resource "azurerm_lb_rule" "web_lb_rule" {
+  name                           = "web-rule"
   protocol                       = "Tcp"
   frontend_port                  = 80
   backend_port                   = 80
